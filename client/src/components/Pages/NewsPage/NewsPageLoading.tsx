6958c6cb532b0f401aa0f4732755aff47f679ea3
Daniel <frosbutte1@gmail.com>
2023-02-28 21:54:03 +0100

    news page loading

--- client/src/components/Pages/NewsPage/NewsPageLoading.tsx
+++ client/src/components/Pages/NewsPage/NewsPageLoading.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 
 export function NewsPageLoading() {
+	const loadingElements: JSX.Element[] = [];
+	for (let i = 0; i < 10; i++) {
+		loadingElements.push(<PostLoading key={i} />);
+	}
 	return (
 		<section className="news">
 			<div className="subsection">
-				<div className="news__loading_posts">
-					<div className="news__loading_post loading_element"></div>
-					<div className="news__loading_post loading_element"></div>
-					<div className="news__loading_post loading_element"></div>
-					<div className="news__loading_post loading_element"></div>
-					<div className="news__loading_post loading_element"></div>
-					<div className="news__loading_post loading_element"></div>
-					<div className="news__loading_post loading_element"></div>
-					<div className="news__loading_post loading_element"></div>
-				</div>
+				<div className="news__loading_posts">{loadingElements}</div>
 			</div>
 		</section>
+	);
+}
+
+function PostLoading() {
+	return (
+		<div className="news__loading_post">
+			<div className="news__loading_post_owner">
+				<div className="news__loading_post_owner_image loading_element"></div>
+				<div className="news__loading_post_owner_name loading_element"></div>
+			</div>
+			<div className="news__loading_post_content loading_element"></div>
+		</div>
 	);
 }
